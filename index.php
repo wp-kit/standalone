43d0bfa05a23769f2f0a6c3e85145565cebafbec
terence1990 <terence@ineedsurgery.com>
2016-10-12 18:27:40 +0100

    Index Fix

--- index.php
+++ index.php
@@ -1,24 +1,45 @@
 <?php
 	
-define('THEME_DIR', getcwd());
+	if( ! function_exists('mb_strtolower') ) {
 	
-/**
- * Tells WordPress not to load the WordPress theme and output it.
- *
- * @var bool
- */	
-define('WP_USE_THEMES', false);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
-
-// make sure composer has been installed
-if( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		function mb_strtolower($string) {
+			
+			return strtolower($string);
+			
+		}
+		
+	}
 	
-	wp_die('Composer has not been installed, try running composer', 'Dependancy Error');
+	if( ! function_exists('mb_strtoupper') ) {
+
+		function mb_strtoupper($string) {
+			
+			return strtoupper($string);
+			
+		}
+		
+	}
 	
-}
-
-
-// Use composer to load the autoloader.
-require __DIR__ . '/vendor/autoload.php';
+	define('THEME_DIR', getcwd());
+		
+	/**
+	 * Tells WordPress not to load the WordPress theme and output it.
+	 *
+	 * @var bool
+	 */	
+	define('WP_USE_THEMES', false);
+	
+	/** Loads the WordPress Environment and Template */
+	require __DIR__ . '/../wp-blog-header.php';
+	
+	// make sure composer has been installed
+	if( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		
+		wp_die('Composer has not been installed, try running composer', 'Dependancy Error');
+		
+	}
+	
+	// Use composer to load the autoloader.
+	require __DIR__ . '/vendor/autoload.php';
+	
+	do_action( 'init' );
